f37026fd639a9f123d9a27ffa2111cf4bc8304a5
rayuhhh <rayuhhh@gmail.com>
2026-01-20 18:30:17 -0600

    rest of files

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+
+import express from "express";
+
+import { handlerReadiness } from "./api/readiness.js";
+
+const app = express();
+const PORT = 8080;
+
+app.use("/app", express.static("./src/app"));
+app.get("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}/app/`);
+});
+
