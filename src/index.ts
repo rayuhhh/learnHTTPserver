6190f50c08f9a484d85ed085719c57b87a31b9bd
rayuhhh <rayuhhh@gmail.com>
2026-01-26 16:54:58 -0600

    renaming, start with serverhits

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,19 @@
 import express from "express";
 
 import { handlerReadiness } from "./api/readiness.js";
-import { middlewareLogResponses } from "./api/log-responses.js";
+import { middlewareLogResponses } from "./api/middleware.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
+
 app.use(middlewareLogResponses);
+
+app.use("/app", express.static("./src/app"));
+
 app.get("/healthz", handlerReadiness);
 
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}/app/`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
 
