44ad261f10cf8d80ec8e66f6407c557d7622721d
rayuhhh <rayuhhh@gmail.com>
2026-01-26 18:46:36 -0600

    messing around with more functionality and html

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -3,7 +3,18 @@ import { config } from "../config.js";
 
 
 export async function handlerMetrics(_: Request, res: Response) {
+    const html= `
+    <html>
+        <body>
+            <h1>Welcome, Chirpy Admin</h1>
+            <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+        </body>
+    </html>
+    `;
+    // <meta charset="utf-8">
+
     res
-        .set("Content-Type", "text/plain; charset=utf-8")
-        .send(`Hits: ${config.fileserverHits}`);
+        .set("Content-Type", "text/html; charset=utf-8")
+        .send(html);
+        //.send(`Hits: ${config.fileserverHits}`);
 }
