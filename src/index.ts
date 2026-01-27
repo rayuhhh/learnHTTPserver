36d41c21fee76def082034c635db064f20787292
rayuhhh <rayuhhh@gmail.com>
2026-01-26 18:29:08 -0600

    modeling decoupled logic: api end points

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,11 @@ const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
+app.use("/app", middlewareMetricsInc, express.static("./src/app"))
 
-app.use("/app", middlewareMetricsInc)
-
-app.use("/app", express.static("./src/app"));
-
-app.get("/healthz", handlerReadiness);
-
-app.get("/metrics", handlerMetrics)
-
-app.get("/reset", handlerReset)
+app.get("/api/healthz", handlerReadiness);
+app.get("/api/metrics", handlerMetrics)
+app.get("/api/reset", handlerReset)
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
